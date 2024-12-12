a774ca0c214afa95183991c1d247f1464ff9954b
rhuandelfino <127902637+rhuandelfino@users.noreply.github.com>
2024-12-12 15:43:20 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import RelogioDigital from './RelogioDigital';
+
+function App() {
+  return (
+    <div className="App">
+      <RelogioDigital/>
+    </div>
+  );
+}
+
+export default App;
